305770b017d6b9616acdea377c0d631870b068ed
dharika909-lgtm <dharika909@gmail.com>
2025-10-20 12:34:15 +0530

    Create productname creator.py

---
+++ productname creator.py
@@ -0,0 +1,5 @@
+#this is a product name creator
+brand_name = input("enter the brand name : ") 
+model_name = input("enter the model name: ") 
+product_name = brand_name + " " + model_name
+print(product_name)
